f7bb2017193698893b53bf94d9966415a18e8a54
Aaron L <daeige@hotmail.com>
2025-07-21 07:27:41 -0400

    Added test functionality for table

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,37 +1,81 @@
+import React, { useState } from 'react';
 import styles from './Table.module.css';
 
 function Table() {
-    return(
-        <div className={styles.routineContainer}>
-            <h2>Your 12 Week Routine</h2>
-            <div className={styles.tableContainer}>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Week</th>
-                            <th>Exercise</th>
-                            <th>Sets</th>
-                            <th>Reps</th>
-                            <th>Weight</th>
-                            <th>Current Max</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Array.from({ length: 12 }, (_, i) => (
-                            <tr key={i}>
-                                <td>{i + 1}</td>
-                                <td>Pulled from API</td>
-                                <td>3</td>
-                                <td>8-10</td>
-                                <td>Calculated by MAX</td>
-                                <td><input></input></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+        const initialWorkoutCycle = [
+        { week: "Test Week", sets: 1, reps: 1, exercises: ["Test1", "Test2", "Test3"]},
+        { week: 1, sets: 4, reps: 6, exercises: ["Test", "Test2", "Test3"]},
+        { week: 2, sets: 4, reps: 6, exercises: ["Test", "Test2", "Test3"]},
+        { week: 3, sets: 4, reps: 5, exercises: ["Test", "Test2", "Test3"]},
+        { week: 4, sets: 4, reps: 6, exercises: ["Test", "Test2", "Test3"]},
+        { week: 5, sets: 3, reps: 5, exercises: ["Test", "Test2", "Test3"]},
+        { week: 6, sets: 4, reps: 4, exercises: ["Test", "Test2", "Test3"]},
+        { week: 7, sets: 4, reps: 3, exercises: ["Test", "Test2", "Test3"]},
+        { week: 8, sets: 3, reps: 5, exercises: ["Test", "Test2", "Test3"]},
+        { week: 9, sets: 4, reps: 3, exercises: ["Test", "Test2", "Test3"]},
+        { week: 10, sets: 3, reps: 2, exercises: ["Test", "Test2", "Test3"]},
+        { week: 11, sets: 3, reps: 1, exercises: ["Test", "Test2", "Test3"]},
+        { week: 12, sets: 4, reps: 3, exercises: ["Test", "Test2", "Test3"]},
+    ]
+
+    const [workoutCycle, setWorkoutCycle] = useState(initialWorkoutCycle);
+
+    const repeatCycle = () => {
+        setWorkoutCycle(prevCycle => [...prevCycle, ...initialWorkoutCycle]);
+    }
+
+  return (
+    <div className={styles.routineContainer}>
+      <h1>Your 12-Week Routine</h1>
+      <table className={styles.tableContainer}>
+        <caption>Cycle 1</caption>
+        <thead>
+          <tr>
+            <th>Week</th>
+            <th>Exercise</th>
+            <th>Sets</th>
+            <th>Reps</th>
+          </tr>
+        </thead>
+        <tbody>
+          {workoutCycle.map((weekData) => (
+            <React.Fragment key={weekData.week}>
+              {weekData.exercises.map((exercise, index) => (
+                <tr key={`${weekData.week}-${index}`}>
+                  {index === 0 && <td rowSpan={weekData.exercises.length}>{weekData.week}</td>}
+                  <td>{exercise.name}</td>
+                  <td>{exercise.sets}</td>
+                  <td>{exercise.reps}</td>
+                </tr>
+              ))}
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+
+      <button className={styles.cycleButton} onClick={repeatCycle}>Repeat Cycle</button>
+    </div>
+  );
 }
 
 export default Table;
+
+
+// Separate into tables of "12 week cycles", add button to repeat cycle
+// Each cycle should have a caption with the cycle number
+// Each week should have each exercise listed, and sets/reps for that week
+// Each exercise should have weight calculation based on 1RM
+
+// Week 1: 4 sets of 6, 70% of 1RM
+// Week 2: 4 sets of 6, 72% of 1RM
+// Week 3: 4 sets of 5, 76% of 1RM
+// Week 4: 4 sets of 6, 60% of 1RM, REST WEEK
+// Week 5: 3 sets of 5, 78% of 1RM
+// Week 6: 4 sets of 4, 83% of 1RM
+// Week 7: 4 sets of 3, 87% of 1RM
+// Week 8: 3 sets of 5, 60% of 1RM, REST WEEK
+// Week 9: 4 sets of 3, 88% of 1RM
+// Week 10: 3 sets of 2, 91% of 1RM
+// Week 11: 3 sets of 1, 97% of 1RM
+// Week 12: 4 sets of 3, 65% of 1RM, REST WEEK
+// Test Week 13: The goal is to hit a higher 1RM for the next routine cycle
