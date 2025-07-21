9357aae50b8efb7d335a6a2098b01584b7b1ae0d
Aaron L <daeige@hotmail.com>
2025-07-21 07:57:53 -0400

    Table functionality

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -3,19 +3,19 @@ import styles from './Table.module.css';
 
 function Table() {
         const initialWorkoutCycle = [
-        { week: "Test Week", sets: 1, reps: 1, exercises: ["Test1", "Test2", "Test3"]},
-        { week: 1, sets: 4, reps: 6, exercises: ["Test", "Test2", "Test3"]},
-        { week: 2, sets: 4, reps: 6, exercises: ["Test", "Test2", "Test3"]},
-        { week: 3, sets: 4, reps: 5, exercises: ["Test", "Test2", "Test3"]},
-        { week: 4, sets: 4, reps: 6, exercises: ["Test", "Test2", "Test3"]},
-        { week: 5, sets: 3, reps: 5, exercises: ["Test", "Test2", "Test3"]},
-        { week: 6, sets: 4, reps: 4, exercises: ["Test", "Test2", "Test3"]},
-        { week: 7, sets: 4, reps: 3, exercises: ["Test", "Test2", "Test3"]},
-        { week: 8, sets: 3, reps: 5, exercises: ["Test", "Test2", "Test3"]},
-        { week: 9, sets: 4, reps: 3, exercises: ["Test", "Test2", "Test3"]},
-        { week: 10, sets: 3, reps: 2, exercises: ["Test", "Test2", "Test3"]},
-        { week: 11, sets: 3, reps: 1, exercises: ["Test", "Test2", "Test3"]},
-        { week: 12, sets: 4, reps: 3, exercises: ["Test", "Test2", "Test3"]},
+        { week: "Test Week", sets: 1, reps: 1, exercises: ["Test1", "Test2", "Test3"], weight: "{input}"},
+        { week: 1, sets: 4, reps: 6, exercises: ["Test1", "Test2", "Test3"], weight: "70% of 1RM"},
+        { week: 2, sets: 4, reps: 6, exercises: ["Test1", "Test2", "Test3"], weight: "72% of 1RM"},
+        { week: 3, sets: 4, reps: 5, exercises: ["Test1", "Test2", "Test3"], weight: "76% of 1RM"},
+        { week: "4 Rest", sets: 4, reps: 6, exercises: ["Test1", "Test2", "Test3"], weight: "60% of 1RM, REST WEEK"},
+        { week: 5, sets: 3, reps: 5, exercises: ["Test1", "Test2", "Test3"], weight: "78% of 1RM"},
+        { week: 6, sets: 4, reps: 4, exercises: ["Test1", "Test2", "Test3"], weight: "83% of 1RM"},
+        { week: 7, sets: 4, reps: 3, exercises: ["Test1", "Test2", "Test3"], weight: "87% of 1RM"},
+        { week: "8 Rest", sets: 3, reps: 5, exercises: ["Test1", "Test2", "Test3"], weight: "60% of 1RM, REST WEEK"},
+        { week: 9, sets: 4, reps: 3, exercises: ["Test1", "Test2", "Test3"], weight: "88% of 1RM"},
+        { week: 10, sets: 3, reps: 2, exercises: ["Test1", "Test2", "Test3"], weight: "91% of 1RM"},
+        { week: 11, sets: 3, reps: 1, exercises: ["Test1", "Test2", "Test3"], weight: "97% of 1RM"},
+        { week: "12 Rest", sets: 4, reps: 3, exercises: ["Test1", "Test2", "Test3"], weight: "65% of 1RM, REST WEEK"},
     ]
 
     const [workoutCycle, setWorkoutCycle] = useState(initialWorkoutCycle);
@@ -35,6 +35,7 @@ function Table() {
             <th>Exercise</th>
             <th>Sets</th>
             <th>Reps</th>
+            <th>Weight</th>
           </tr>
         </thead>
         <tbody>
@@ -43,17 +44,20 @@ function Table() {
               {weekData.exercises.map((exercise, index) => (
                 <tr key={`${weekData.week}-${index}`}>
                   {index === 0 && <td rowSpan={weekData.exercises.length}>{weekData.week}</td>}
-                  <td>{exercise.name}</td>
-                  <td>{exercise.sets}</td>
-                  <td>{exercise.reps}</td>
+                  <td>{weekData.exercises}</td>
+                  <td>{weekData.sets}</td>
+                  <td>{weekData.reps}</td>
+                  <td>{weekData.weight}</td>
                 </tr>
               ))}
             </React.Fragment>
           ))}
         </tbody>
       </table>
-
-      <button className={styles.cycleButton} onClick={repeatCycle}>Repeat Cycle</button>
+        <div className={styles.buttonContainer}>
+            <button className={styles.tableButton} onClick={repeatCycle}>Repeat Cycle</button>
+            <button className={styles.tableButton} onClick={() => alert('Calculate functionality not implemented yet.')}>Calculate Weight</button>
+        </div>
     </div>
   );
 }
@@ -61,7 +65,7 @@ function Table() {
 export default Table;
 
 
-// Separate into tables of "12 week cycles", add button to repeat cycle
+// Separate into tables of "12 week cycles" starting with a test week, add button to repeat cycle
 // Each cycle should have a caption with the cycle number
 // Each week should have each exercise listed, and sets/reps for that week
 // Each exercise should have weight calculation based on 1RM
